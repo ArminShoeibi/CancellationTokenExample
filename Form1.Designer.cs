e07bee16d619ca4c103e81f86d56488266b44632
a.shoeibi <a.shoeibi@alibaba.ir>
2021-08-18 01:21:23 +0430

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace CancellationTokenExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 97);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(250, 50);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Location = new System.Drawing.Point(407, 97);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(250, 50);
+            this.CancelButton.TabIndex = 1;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 213);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button CancelButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows.Forms;
+
+
+namespace CancellationTokenExample
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 
+        /// In fact, when you cancel code, there are three possibilities: 
+        /// 1) It may respond tothe cancellation request (throwing OperationCanceledException).
+        /// 2) It may finish successfully.
+        /// 3) It may finish with an error unrelated to the cancellation (throwing a different exception).
+        /// 
+        /// </summary>
+        private CancellationTokenSource _cancellationTokenSource;
+        public Form1()
+        {
+            InitializeComponent();
+            CancelButton.Enabled = false;
+            _cancellationTokenSource = new();
+        }
+
+        private async void StartButton_Click(object sender, EventArgs e)
+        {
+            StartButton.Enabled = false;
+            CancelButton.Enabled = true;
+
+            try
+            {
+                await Task.Delay(TimeSpan.FromSeconds(5), _cancellationTokenSource.Token);
+                MessageBox.Show("Delay completed successfully.");
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Delay was canceled.");
+                //throw;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Delay completed with error.");
+                throw;
+            }
+            finally
+            {
+                StartButton.Enabled = true;
+                CancelButton.Enabled = false;
+                _cancellationTokenSource = new(); // comment this line at the first. after that if you want interactive UI uncomment this line.
+            }
+
+
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            _cancellationTokenSource.Cancel();
+            CancelButton.Enabled = false;
+        }
+    }
+}
